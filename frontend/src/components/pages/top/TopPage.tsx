38625baf3c131b6f37871ae9abe311775bb68623
V01d42 <89440437+V01d42@users.noreply.github.com>
2024-08-09 10:30:35 +0900

    202408091030

--- frontend/src/components/pages/top/TopPage.tsx
+++ frontend/src/components/pages/top/TopPage.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
-
-import { useState, useMemo } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import Map, { Marker, NavigationControl } from 'react-map-gl';
+import Map, { Marker, NavigationControl, Popup } from 'react-map-gl';
 import styles from './TopPage.module.scss';
 import { shelterRepository } from '@/libs/repository/firebase';
-import { onScroll } from '@/hooks/useScroll';
+import { fetchGeocode } from '@/libs/services/getCoordinate';
+import { FlexBox, Typography } from '@/components/common';
 
 type Pin = {
   title: string;
@@ -16,9 +15,9 @@ type Pin = {
 };
 
 const renderStars = (rating: number) => {
-  const fullStars = Math.floor(rating);
+  const fullStars = Math.floor(Math.max(0, rating));
   const halfStar = rating % 1 !== 0 ? 1 : 0;
-  const emptyStars = 5 - fullStars - halfStar;
+  const emptyStars = Math.max(0, 5 - fullStars - halfStar);
 
   return (
     <div className={styles.stars}>
@@ -37,9 +36,36 @@ const renderStars = (rating: number) => {
   );
 };
 
+// Haversine formula
+const haversineDistance = (
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number,
+) => {
+  const R = 6371; //Earth radius in km
+  const dLat = ((lat2 - lat1) * Math.PI) / 180;
+  const dLon = ((lon1 - lon2) * Math.PI) / 180;
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos((lat1 * Math.PI) / 180) *
+      Math.cos((lat2 * Math.PI) / 180) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c; // Distance in km
+};
+
 const TopPage: React.FC = () => {
   const [pinList, setPinList] = useState<Pin[]>([]);
   const [popupInfo, setPopupInfo] = useState<Pin | null>(null);
+  const [inputAddress, setInputAddress] = useState('');
+  const [inputLocation, setInputLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
+  const [topShelters, setTopShelters] = useState<Pin[]>([]);
+  const [searchClicked, setSearchClicked] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -63,6 +89,38 @@ const TopPage: React.FC = () => {
     fetchData();
   }, []);
 
+  const handleSearch = async () => {
+    if (inputAddress) {
+      const { latitude, longitude } = await fetchGeocode(
+        inputAddress,
+        'JP',
+        'ja',
+        import.meta.env.VITE_MAPBOX_TOKEN,
+      );
+      setInputLocation({ latitude, longitude });
+      setSearchClicked(true);
+    }
+  };
+
+  useEffect(() => {
+    if (inputLocation) {
+      const sheltersWithin5Km = pinList.filter((pin) => {
+        const distance = haversineDistance(
+          inputLocation.latitude,
+          inputLocation.longitude,
+          pin.latitude,
+          pin.longitude,
+        );
+        return distance <= 5;
+      });
+
+      const sortedShelters = sheltersWithin5Km.sort(
+        (a, b) => b.score - a.score,
+      );
+      setTopShelters(sortedShelters.slice(0, 5));
+    }
+  }, [inputLocation, pinList]);
+
   const pins = useMemo(
     () =>
       pinList.map((data, index) => (
@@ -74,11 +132,27 @@ const TopPage: React.FC = () => {
           onClick={(e) => {
             e.originalEvent.stopPropagation();
             setPopupInfo(data);
-            onScroll('container', 'bottom');
           }}
-        />
+        >
+          {popupInfo?.title === data.title && (
+            <Popup
+              longitude={data.longitude}
+              latitude={data.latitude}
+              anchor='top'
+              closeOnClick={false}
+              onClose={() => setPopupInfo(null)}
+              className={styles.popup}
+            >
+              <div>
+                <h4>{data.title}</h4>
+                <p>評価: {renderStars(data.score)}</p>
+                <p>住所: {data.address}</p>
+              </div>
+            </Popup>
+          )}
+        </Marker>
       )),
-    [pinList],
+    [pinList, popupInfo],
   );
 
   return (
@@ -87,8 +161,8 @@ const TopPage: React.FC = () => {
       <div className={styles.mapContainer}>
         <Map
           initialViewState={{
-            longitude: 139.76711,
-            latitude: 35.68074,
+            longitude: 140.95831,
+            latitude: 38.235424,
             zoom: 12,
           }}
           style={{ position: 'absolute', top: 0, bottom: 0, width: '100%' }}
@@ -99,25 +173,69 @@ const TopPage: React.FC = () => {
           <NavigationControl />
         </Map>
       </div>
+      <div className={styles.rankingContainer}>
+        <div className={styles.bg} />
+        <FlexBox justifyContent='center' gap='8px'>
+          <div className={styles.title}>避難所検索機能</div>
+        </FlexBox>
+        <FlexBox
+          flexDirection='column'
+          gap='8px'
+          className={styles.inputContainer}
+        >
+          <FlexBox gap='24px'>
+            <input
+              type='text'
+              value={inputAddress}
+              onChange={(e) => setInputAddress(e.target.value)}
+              placeholder='住所を入力してください'
+              className={styles.input}
+            />
+            <button onClick={handleSearch} className={styles.searchButton}>
+              <Typography color='w1' fontSize='12px'>
+                検索
+              </Typography>
+            </button>
+          </FlexBox>
+          <Typography fontWeight={300} fontSize='12px' color='b2'>
+            入力した住所の半径5km以内の避難所Top5を表示します
+          </Typography>
+        </FlexBox>
 
-      <div id='container' className={styles.contentWrapper}>
-        <div className={styles.infoTitle}>避難所情報</div>
-        <p className={styles.subTitle}>備蓄の充実度合いが評価になります</p>
-        <div className={styles.redFrameContainer}>
-          <div className={styles.frameContent}>
-            <div className={styles.name}>
-              {popupInfo ? popupInfo.title : '未選択'}
-            </div>
-            <div className={styles.starsContainer}>
-              <div>評価</div>
-              {popupInfo ? renderStars(popupInfo.score) : '-'}
-              <div>{popupInfo ? popupInfo.score : ''}</div>
-            </div>
-            <div className={styles.address}>
-              住所：{popupInfo ? popupInfo.address : '未選択'}
-            </div>
+        {searchClicked && topShelters.length > 0 ? (
+          <div className={styles.topShelters}>
+            <Typography
+              fontWeight={600}
+              fontSize='20px'
+              className={styles.shelterTitle}
+            >
+              5km以内の避難所
+            </Typography>
+            {topShelters.map((shelter, index) => (
+              <div key={index} className={styles.shelterInfo}>
+                <FlexBox
+                  flexDirection='column'
+                  gap='4px'
+                  className={styles.shelterCard}
+                >
+                  <Typography fontWeight={600} fontSize='16px'>
+                    {shelter.title}
+                  </Typography>
+                  <Typography fontSize='12px'>評価: {shelter.score}</Typography>
+                  <Typography fontSize='12px'>
+                    住所: {shelter.address}
+                  </Typography>
+                </FlexBox>
+              </div>
+            ))}
           </div>
-        </div>
+        ) : (
+          <FlexBox justifyContent='center' className={styles.noResult}>
+            <Typography color='b3' fontWeight={600} fontSize='20px'>
+              検索結果なし
+            </Typography>
+          </FlexBox>
+        )}
       </div>
     </div>
   );
